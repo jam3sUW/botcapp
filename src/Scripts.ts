3401a4f2b5570ba3f014da63b69bb300bfef4852
JSLarson <jamesladmissions@gmail.com>
2026-08-28 17:01:41 -0700

    Add helper function to get character type counts

--- src/Scripts.ts
+++ src/Scripts.ts
@@ -26,3 +26,29 @@ export function loadScript(data: any[]) : Script {
     }
     return {name: name, author: author, bootlegger: bootlegger, characterIds: charIds}
 }
+
+interface CharacterTypeCount {
+    townsfolk: number,
+    outsiders: number,
+    minions: number,
+    demons: number
+}
+
+const CHARACTER_TYPE_COUNTS: Record<number, CharacterTypeCount> = {
+    5: { townsfolk: 3, outsiders: 0, minions: 1, demons: 1 },
+    6: { townsfolk: 3, outsiders: 1, minions: 1, demons: 1 },
+    7: { townsfolk: 5, outsiders: 0, minions: 1, demons: 1 },
+    8: { townsfolk: 5, outsiders: 1, minions: 1, demons: 1 },
+    9: { townsfolk: 5, outsiders: 2, minions: 1, demons: 1 },
+    10: { townsfolk: 7, outsiders: 0, minions: 2, demons: 1 },
+    11: { townsfolk: 7, outsiders: 1, minions: 2, demons: 1 },
+    12: { townsfolk: 7, outsiders: 2, minions: 2, demons: 1 },
+    13: { townsfolk: 9, outsiders: 0, minions: 3, demons: 1 },
+    14: { townsfolk: 9, outsiders: 1, minions: 3, demons: 1 },
+    15: { townsfolk: 9, outsiders: 2, minions: 3, demons: 1 },
+}
+
+export function getCharacterTypeCounts(playerCount: number) : CharacterTypeCount {
+    playerCount = Math.max(5, Math.min(playerCount, 15))
+    return CHARACTER_TYPE_COUNTS[playerCount]!
+}
