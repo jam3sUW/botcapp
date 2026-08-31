2336ebf62252a05c4d956d11f3b930fe5d3a44c6
JSLarson <jamesladmissions@gmail.com>
2026-08-31 16:29:17 -0700

    Finish rename of PlayerToken.tsx for real this time

--- src/InteractiveGrimoire.tsx
+++ src/InteractiveGrimoire.tsx
@@ -4,11 +4,10 @@ import { aliveCount, availableVotes, blockVotes, exileVotes, firstNightOrder, hi
 import ScriptLoader from "./ScriptLoader";
 import { getCharacter, fabledLorics, type Character } from "./Characters";
 import BluffManager from "./BluffManager";
-import Token from "./PlayerToken";
+import PlayerToken from "./PlayerToken";
 import { getInPlayJinxes } from "./Jinxes";
 import "./InteractiveGrimoire.css"
 import CharacterSetup from "./CharacterSetup";
-import PlayerToken from "./PlayerToken";
 
 function InteractiveGrimoire() {
     const [{ present: state, past, future }, dispatch] = useReducer(historyReducer, { past: [], present: generateInitialGrimState(), future: []})
