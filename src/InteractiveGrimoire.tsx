53b1f30c603fa369a54ac3d292941fa6d07876eb
JSLveil <jslpacoyo@gmail.com>
2026-09-03 20:30:54 -0700

    Minor visual pass

--- src/InteractiveGrimoire.tsx
+++ src/InteractiveGrimoire.tsx
@@ -15,6 +15,7 @@ function InteractiveGrimoire() {
     const [{ present: state, past, future }, dispatch] = useReducer(historyReducer, { past: [], present: generateInitialGrimState(), future: []})
     const sortedTokens = [...state.tokens].sort((a, b) => (a.seat ?? 999) - (b.seat ?? 999))
     const inPlayJinxes = getInPlayJinxes(state)
+    const hasTokens = state.tokens.length !== 0 || state.fabledLorics.length !== 0
 
     return (
         <div>
@@ -23,7 +24,7 @@ function InteractiveGrimoire() {
 
             <ScriptLoader dispatch={dispatch}/>
             <p>{state.script.name}</p>
-            
+
             <CharacterSetup dispatch={dispatch} state={state}/>
 
             <TokenAdder dispatch={dispatch} state={state}/>
@@ -33,60 +34,62 @@ function InteractiveGrimoire() {
             <FabledLoricAdder dispatch={dispatch} state={state}/>
 
             <button onClick={() => dispatch({ type: "clear"})}>Clear</button>
-            
-            <p>Players: {state.tokens.length}</p>
-            <p>Alive: {aliveCount(state.tokens)}</p>
-            <p>Available votes: {availableVotes(state.tokens)}</p>
-            <p>Votes to condemn: {blockVotes(state.tokens)}</p>
-            <p>Votes to exile: {exileVotes(state.tokens)}</p>
+            {hasTokens &&
+                <>
+                    <p>Players: {state.tokens.length}</p>
+                    <p>Alive: {aliveCount(state.tokens)}</p>
+                    <p>Available votes: {availableVotes(state.tokens)}</p>
+                    <p>Votes to condemn: {blockVotes(state.tokens)}</p>
+                    <p>Votes to exile: {exileVotes(state.tokens)}</p>
 
-            <h2>Tokens (click each for options):</h2>
-            <ul className="token-list">
-                {sortedTokens.map(token => (
-                    <PlayerToken key={token.id} dispatch={dispatch} token={token} state={state}/>
-                ))}
-            </ul>
-            
-            <h2>First night:</h2>
-            <ul>
-                {firstNightOrder(state.tokens, state.fabledLorics).map(id => {
-                    const character = getCharacter(id)
-                    if (!character) return null
-                    return (
-                        <li key={id}>
-                            <strong>{character.name}: </strong>
-                            {character.firstNightInstruction}
-                        </li>
-                    )
-                })}
-            </ul>
-            <h2>Other night:</h2>
-            <ul>
-                {otherNightOrder(state.tokens, state.fabledLorics).map(id => {
-                    const character = getCharacter(id)
-                    if (!character) return null
-                    return (
-                        <li key={id}>
-                            <strong>{character.name}: </strong>
-                            {character.otherNightInstruction}
-                        </li>
-                    )
-                })}
-            </ul>
-
+                    <h2>Tokens (click each for options):</h2>
+                    <ul className="token-list">
+                        {sortedTokens.map(token => (
+                            <PlayerToken key={token.id} dispatch={dispatch} token={token} state={state}/>
+                        ))}
+                    </ul>
+                    
+                    <h2>First night:</h2>
+                    <ul>
+                        {firstNightOrder(state.tokens, state.fabledLorics).map(id => {
+                            const character = getCharacter(id)
+                            if (!character) return null
+                            return (
+                                <li key={id}>
+                                    <strong>{character.name}: </strong>
+                                    {character.firstNightInstruction}
+                                </li>
+                            )
+                        })}
+                    </ul>
+                    <h2>Other night:</h2>
+                    <ul>
+                        {otherNightOrder(state.tokens, state.fabledLorics).map(id => {
+                            const character = getCharacter(id)
+                            if (!character) return null
+                            return (
+                                <li key={id}>
+                                    <strong>{character.name}: </strong>
+                                    {character.otherNightInstruction}
+                                </li>
+                            )
+                        })}
+                    </ul>
+                </>
+            }
            <BluffManager dispatch={dispatch} state={state}/>
 
            {state.fabledLorics.length !== 0 &&
                 <>
                     <h2>Fabled/Lorics</h2>
-                    <ul>
-                        {state.fabledLorics.map(id => (
-                            <li key={id}>
-                                {getCharacter(id)!.name}
-                                <button onClick={() => dispatch({ type: "removeFabledLoric", characterId: id })}>Remove</button>
-                            </li>
-                        ))}
-                    </ul>
+            <ul>
+                {state.fabledLorics.map(id => (
+                    <li key={id}>
+                        {getCharacter(id)!.name}
+                        <button onClick={() => dispatch({ type: "removeFabledLoric", characterId: id })}>Remove</button>
+                    </li>
+                ))}
+            </ul>
                 </>
             }
 
